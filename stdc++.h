e5a30ba15dfc9791c9026896a10cc98ac130893c
Kwadwo A Bempah <kwadwoasiedu@bempah.com>
2023-10-15 00:44:57 -0400

    adder super-header file

---
+++ stdc++.h
@@ -0,0 +1,30 @@
+/*
+    For education purposes only.
+    (M) -> Macro Constant
+*/
+
+#include <iostream>
+/* cin, cout */
+
+#include <cstring>
+/* strlen, strcpy, strncpy, strstr, strcmp, strncmp */
+
+#include <cctype>
+/* tolower, toupper, isalnum, isalpha, isdigit, isupper, islower, ...*/
+
+#include <cstdlib>
+/* size_t, malloc, free, exit, EXIT_SUCCESS(M), EXIT_FAILURE(M), rand, srand, bsearch, qsort,
+    RAND_MAX(M), NULL(M), abs, div */
+
+#include <string>
+/* std::string */
+
+#include <vector>
+/* std::vector */
+
+#include <memory>
+/* std::unique_ptr, std::shared_ptr, std::weak_ptr */
+
+#include <new>
+/* std::bad_alloc */
+
